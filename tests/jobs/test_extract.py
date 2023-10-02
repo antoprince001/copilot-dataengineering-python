112005cd9fefe662dba3886d6326c2c19da4d0ec
antoprince001 <antoprince001@gmail.com>
2023-10-03 00:32:39 +0530

    Update tests

--- tests/jobs/test_extract.py
+++ tests/jobs/test_extract.py
@@ -8,4 +8,6 @@ def test_read_csv_file(spark_session):
 
     assert df.count() == 51290
     assert len(df.columns) == 8
-    assert df.columns == ['InvoiceNo', 'StockCode', 'Description', 'Quantity', 'InvoiceDate', 'UnitPrice', 'CustomerID', 'Country']
+    assert df.columns == ['InvoiceNo', 'StockCode', 'Description',
+                          'Quantity', 'InvoiceDate', 'UnitPrice',
+                          'CustomerID', 'Country']
